92fddcbcd6348ec9abcf45d9b5ba423193cc5790
ck6jp6fu6 <ck6jp6fu6@gmail.com>
2017-04-12 13:03:04 +0800

    init commit

---
+++ Assets/GameUIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class GameUIManager : MonoBehaviour {
+
+	public Image BlackCover;
+	public Image BloodBlur;
+	public Text HPText;
+
+	void Start(){
+		BlackCover.color = Color.black;
+		DOTween.To (() => BlackCover.color, (x) => BlackCover.color = x, new Color (0, 0, 0, 0), 1f);
+	}
+
+	Tweener tweenAnimation;
+
+	public void PlayHitAnimation(){
+		if (tweenAnimation != null)
+			tweenAnimation.Kill ();
+
+		BloodBlur.color = Color.white;
+		tweenAnimation = DOTween.To (() => BloodBlur.color, (x) => BloodBlur.color = x, new Color (1, 1, 1, 0), 0.5f);
+	}
+
+	public void PlayDiedAnimation(){
+		BloodBlur.color = Color.white;
+		DOTween.To (() => BlackCover.color, (x) => BlackCover.color = x, new Color (0, 0, 0, 1), 1f).
+		SetDelay (3).OnComplete (() => {
+			DOTween.To (() => BloodBlur.color, (x) => BloodBlur.color = x, new Color (1, 1, 1, 0), 0.5f).SetDelay (3).OnComplete (() => {
+				SceneManager.LoadScene ("Main");
+			});
+		});
+	}
+
+	public void SetHP(int hp){
+		HPText.text = "HP : " + hp;
+	}
+}
---
+++ Assets/MonsterSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour {
+
+	public GameObject MonsterCandidate1;
+	public GameObject MonsterCandidate2;
+	public List<Transform> SpawnPoint;
+	public GameObject initFollowTarget;
+
+	public float SpawnMonsterTime = 100;
+	private float spwanCounter = 0;
+
+	// Update is called once per frame
+	void Update () {
+		spwanCounter += Time.deltaTime;
+
+		if (spwanCounter >= SpawnMonsterTime) {
+			spwanCounter = 0;
+			float ran = Random.value;
+			if (ran < 0.5) {
+				GameObject newMonster = GameObject.Instantiate (MonsterCandidate1);
+				newMonster.GetComponent<MonsterScript> ().FollowTarget = initFollowTarget;
+				newMonster.transform.position = SpawnPoint [Random.Range (0, SpawnPoint.Count)].position;
+			}
+			else{
+				GameObject newMonster = GameObject.Instantiate (MonsterCandidate2);
+				newMonster.GetComponent<AnemyController> ().FollowTarget = initFollowTarget;
+				newMonster.transform.position = SpawnPoint [Random.Range (0, SpawnPoint.Count)].position;
+			}
+		}
+	}
+}
